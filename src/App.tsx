2414052ac04037778c979bfe065b8c949f01aab1
snguyen56 <101228542+snguyen56@users.noreply.github.com>
2023-11-28 17:33:25 -0500

    add and configure tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
 import Logo from "./assets/logo.svg";
 
 function App() {
-  return <main></main>;
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </main>
+  );
 }
 
 export default App;
