2a0a01068f0eb1ee1c9983bc7268b6925a587247
snguyen56 <101228542+snguyen56@users.noreply.github.com>
2023-11-29 00:42:15 -0500

    add dummy data to daily table

--- src/components/layouts/Forecast.tsx
+++ src/components/layouts/Forecast.tsx
@@ -7,10 +7,10 @@ type Props = {
 
 export default function Forecast({ weatherData }: Props) {
   return (
-    <section className="bg-background flex-1 px-10 py-5">
+    <section className="flex-1 bg-background px-10 py-5">
       <SearchBar />
       <h2 className="mb-4 mt-20 text-4xl font-bold">Hourly Forecast</h2>
-      <Card className="bg-primary flex gap-1 p-2"></Card>
+      <Card className="flex gap-1 bg-primary p-2"></Card>
       <h2 className="mb-4 text-4xl font-bold ">Daily Forecast</h2>
       <Card className="bg-primary p-2">
         <table className="table-auto">
@@ -18,10 +18,20 @@ export default function Forecast({ weatherData }: Props) {
             <tr>
               <th>Weekday</th>
               <th>Weather</th>
+              <th>Rain Probability</th>
               <th>High</th>
               <th>Low</th>
             </tr>
           </thead>
+          <tbody>
+            <tr>
+              <td>Monday</td>
+              <td></td>
+              <td>50%</td>
+              <td>50°</td>
+              <td>50°</td>
+            </tr>
+          </tbody>
         </table>
       </Card>
     </section>
