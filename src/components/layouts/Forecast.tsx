8b943ffd70251368c66e729d9e6b9e269e5ef43d
snguyen56 <101228542+snguyen56@users.noreply.github.com>
2023-11-29 20:20:28 -0500

    integrated hourly and daily components

--- src/components/layouts/Forecast.tsx
+++ src/components/layouts/Forecast.tsx
@@ -12,9 +12,28 @@ export default function Forecast({ weatherData }: Props) {
     <section className="flex-1 bg-background px-10 py-5">
       <SearchBar />
       <h2 className="mb-4 mt-20 text-4xl font-bold">Hourly Forecast</h2>
-      <Card className="flex gap-1 bg-primary p-2"></Card>
+      <Card className="flex justify-between gap-1 bg-primary p-2 px-8">
+        <IconContext.Provider value={{ size: "50" }}>
+          {weatherData.hourly.time
+            .slice(0, 7)
+            .map((time: string, index: number) => (
+              <div className="flex flex-col gap-1 text-center" key={time}>
+                <h3 className="mb-2 font-bold">
+                  {new Date(time).toLocaleTimeString([], { hour: "2-digit" })}
+                </h3>
+                {
+                  WeatherCodes.find(
+                    ({ code }) =>
+                      code === weatherData.hourly.weather_code[index],
+                  )?.dayIcon
+                }
+                <p>{weatherData.hourly.temperature_2m[index]}</p>
+              </div>
+            ))}
+        </IconContext.Provider>
+      </Card>
       <h2 className="mb-4 text-4xl font-bold ">Daily Forecast</h2>
-      <Card className="bg-primary p-2">
+      <Card className="bg-primary p-4">
         <IconContext.Provider value={{ size: "50" }}>
           <table className="w-full table-auto text-xl">
             <thead>
@@ -27,13 +46,24 @@ export default function Forecast({ weatherData }: Props) {
               </tr>
             </thead>
             <tbody>
-              <tr className="border-t border-black">
-                <td>Monday</td>
-                <td>{WeatherCodes.find(({ code }) => code === 0)?.dayIcon}</td>
-                <td>50%</td>
-                <td>50°</td>
-                <td>50°</td>
-              </tr>
+              {weatherData.daily.time.map((day: string, index: number) => (
+                <tr className="border-t border-black">
+                  <td>{day}</td>
+                  <td>
+                    {
+                      WeatherCodes.find(
+                        ({ code }) =>
+                          code === weatherData.daily.weather_code[index],
+                      )?.dayIcon
+                    }
+                  </td>
+                  <td>
+                    {weatherData.daily.precipitation_probability_max[index]}
+                  </td>
+                  <td>{weatherData.daily.temperature_2m_max[index]}</td>
+                  <td>{weatherData.daily.temperature_2m_min[index]}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </IconContext.Provider>
